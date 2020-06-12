cdf79c21fad95a0ff4d57f28580c1397e456f049
djadji.traore75 <djadji.traore75@gmail.com>
2020-06-12 15:46:30 +0200

    Add some lib to the project

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:noticetracker/signIn/SignInScreen.dart';
 import 'package:noticetracker/HomePage.dart';
+import 'package:noticetracker/signUp/SignUpScreen.dart';
 
 void main() => runApp(MaterialApp(
   home: SignInScreen(),
   routes:  <String, WidgetBuilder>{
     "/home": (BuildContext context) => new HomePage(),
+    "/signUp": (BuildContext context) => new SignUpScreen(),
   },
 ));
 
