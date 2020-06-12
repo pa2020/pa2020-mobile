b79b83ead80a20b634f80ae8be58bbd9e8eaa4d7
djadji.traore75 <djadji.traore75@gmail.com>
2020-06-12 15:46:09 +0200

    Update the enum, to contain the id of the users & updated some method for the user

--- lib/user/UserDto.dart
+++ lib/user/UserDto.dart
@@ -1,4 +1,65 @@
 
 class UserDto{
 
+  String _email;
+  String _firstname;
+  String _lastname;
+  String _password;
+  List<String> _role;
+  String _username;
+
+  UserDto(this._email, this._firstname, this._lastname, this._password,
+      this._role, this._username);
+
+  String get username => _username;
+
+  set username(String value) {
+    _username = value;
+  }
+
+  List<String> get role => _role;
+
+  set role(List<String> value) {
+    _role = value;
+  }
+
+  String get password => _password;
+
+  set password(String value) {
+    _password = value;
+  }
+
+  String get lastname => _lastname;
+
+  set lastname(String value) {
+    _lastname = value;
+  }
+
+  String get firstname => _firstname;
+
+  set firstname(String value) {
+    _firstname = value;
+  }
+
+  String get email => _email;
+
+  set email(String value) {
+    _email = value;
+  }
+
+  @override
+  String toString() {
+    return 'UserDto{_email: $_email, _firstname: $_firstname, _lastname: $_lastname, _password: $_password, _role: $_role, _username: $_username}';
+  }
+
+  factory UserDto.fromJson(Map<String, dynamic> json) {
+    return UserDto(
+      json["email"],
+      json["firstname"],
+      json["lastname"],
+      json["password"],
+      json["role"],
+      json["username"]);
+  }
+
 }
--- lib/user/UserService.dart
+++ lib/user/UserService.dart
@@ -1,13 +1,143 @@
 
-import 'package:noticetracker/user/LoginDto.dart';
+import 'dart:convert';
+
+import 'package:enum_to_string/enum_to_string.dart';
+import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:noticetracker/enumerate/SharedPrefEnum.dart';
+import 'package:noticetracker/signIn/LoginForm.dart';
+import 'package:noticetracker/user/UserDto.dart';
+import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
+
+import '../HomePage.dart';
 
 class UserService{
 
-  static bool loginUser(LoginDto loginDto){
-    return true;
+  static Future<http.Response> _loginUserRequest(LoginForm login) async {
+
+    return http.post("https://pa2020-api.herokuapp.com/api/v1/auth/signin",
+        body : jsonEncode(<String, String>{
+          "password": login.password,
+          "username": login.username
+        }),
+        headers: <String, String>{
+      'Content-Type': 'application/json; charset=UTF-8',
+    });
   }
 
-  registerUser(){
-    return true;
+  static Future<http.Response> _registerUserRequest(UserDto userDto) async {
+
+    return http.post("https://pa2020-api.herokuapp.com/api/v1/auth/signup",
+        body : jsonEncode(<String, String>{
+          "email": userDto.email,
+          "firstname": userDto.firstname,
+          "lastname": userDto.lastname,
+          "password": userDto.password,
+          "role": "["+listToString(userDto.role)+"]",
+          "username": userDto.username
+        }),
+        headers: <String, String>{
+          'Content-Type': 'application/json',
+    });
+  }
+
+
+  static Future<http.Response> _updateUserProfile(UserDto userDto){
+
+    return http.put("https://pa2020-api.herokuapp.com/api/v1/auth/signup", body: jsonEncode(userDto), headers: <String, String>{
+      'Content-Type': 'application/json; charset=UTF-8',
+    });
+  }
+
+  static Future<UserDto> getUserInfo() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    int id = prefs.getInt(EnumToString.parse(SharedPrefEnum.id));
+    String token = prefs.getString(EnumToString.parse(SharedPrefEnum.token));
+    var res = http.put("https://pa2020-api.herokuapp.com/api/v1/users/$id", headers: <String, String>{
+      "Content-Type": "application/json; charset=UTF-",
+      "Authorization": "Bearer "+token,
+    });
+    return new UserDto("test@mail.com", "firstname", "lastname", "12345", new List.from(["client"]), "username");
+  }
+
+
+  static Future<void> logUser(LoginForm login, BuildContext context) async {
+    var response = await _loginUserRequest(login);
+    print('Response status: ${response.statusCode}');
+    print('Response body: ${response.body}');
+    if(response.statusCode==200){
+      Fluttertoast.showToast(msg: "Logged in");
+      var jsonRes = json.decode(response.body);
+      _saveJsonInfoInSharedPref(jsonRes, login.username, login.password);
+      Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (BuildContext context) => new HomePage()));
+    }else  {
+      Fluttertoast.showToast(msg: "Can't loggin");
+    }
+  }
+
+  static Future<void> checkIfUserAlreadyLoggedIn(BuildContext context) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    String token = prefs.getString(EnumToString.parse(SharedPrefEnum.token));
+    if(token==null || token=="")
+      return;
+    String username = prefs.getString(EnumToString.parse(SharedPrefEnum.username));
+    if(username==null || username=="")
+      return;
+    String password = prefs.getString(EnumToString.parse(SharedPrefEnum.password));
+    if(password==null || password=="")
+      return;
+
+    UserService.logUser(new LoginForm(username, password), context);
+  }
+
+
+  static registerUser(UserDto userDto) async {
+    var response = await _registerUserRequest(userDto);
+    print('Response status: ${response.statusCode}');
+    var res = response.body;
+    print(res);
+    if( response.statusCode!=200){
+      Fluttertoast.showToast(msg: "Cant registered user");
+      throw new Error();
+    }else {
+      Fluttertoast.showToast(msg: "User registered");
+      var jsonRes = jsonDecode(response.body);
+      _saveJsonInfoInSharedPref(jsonRes, userDto.username, userDto.password);
+    }
+  }
+
+  static updateProfile(UserDto userDto) async {
+    var response = await _updateUserProfile(userDto);
+    print('Response status: ${response.statusCode}');
+    print('Response body: ${response.body}');
+    if( response.statusCode!=200){
+      throw new Error();
+    }else {
+      Fluttertoast.showToast(msg: "Profile updated");
+      var jsonRes = jsonDecode(response.body);
+      _saveJsonInfoInSharedPref(jsonRes, userDto.username, userDto.password);
+    }
+  }
+
+
+  static _saveJsonInfoInSharedPref(Map<String, dynamic> json, String username, String password) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.setString(EnumToString.parse(SharedPrefEnum.username), username);
+    prefs.setString(EnumToString.parse(SharedPrefEnum.password), password);
+    prefs.setString(EnumToString.parse(SharedPrefEnum.token), json["token"]);
+    prefs.setInt(EnumToString.parse(SharedPrefEnum.id), json["id"]);
+  }
+
+  static _fromJsonToUserDto(Map<String, dynamic> json){
+
+  }
+  
+  static String listToString(List<String> list){
+    String concat = "";
+    list.forEach((item) {
+      concat+=item+",";
+    });
+    return concat.substring(0,concat.length-1);
   }
 }
